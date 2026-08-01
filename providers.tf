d5224e23719ca6fa3b57874bcf5641e88cc28358
ballakeita <ballakei070@gmail.com>
2026-08-01 16:20:06 +0200

    add check environnement step

--- providers.tf
+++ providers.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.81.0"
+      version = "4.90.0"
+      #version = "~> 4.90.0"
     }
   }
 }
